731d46130a5db6198cb937134508e4bb0ea75835
maarten <maarten.diederik@student.hu.nl>
2018-01-30 23:07:19 +0100

    implemented own mjpg server

--- module/main.py
+++ module/main.py
@@ -5,6 +5,68 @@ import socket
 import json
 import netifaces
 from time import sleep
+import cv2
+from PIL import Image
+import threading
+from http.server import BaseHTTPRequestHandler,HTTPServer
+from socketserver import ThreadingMixIn
+from io import StringIO,BytesIO
+import time
+capture=None
+streaming_server_on = False
+
+class CamHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith('.mjpg'):
+            self.send_response(200)
+            self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            while True:
+                try:
+                    rc,img = capture.read()
+                    if not rc:
+                        continue
+                    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+                    jpg = Image.fromarray(imgRGB)
+                    tmpFile = BytesIO()
+                    jpg.save(tmpFile,'JPEG')
+                    self.wfile.write("--jpgboundary".encode())
+                    self.send_header('Content-type','image/jpeg')
+                    self.send_header('Content-length',str(tmpFile.getbuffer().nbytes))
+                    self.end_headers()
+                    jpg.save(self.wfile,'JPEG')
+                    time.sleep(0.05)
+                except KeyboardInterrupt:
+                    break
+            return
+        if self.path.endswith('.html'):
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write('<html><head></head><body>'.encode())
+            self.wfile.write('<img src="http://127.0.0.1:10088/cam.mjpg"/>'.encode())
+            self.wfile.write('</body></html>'.encode())
+            return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+def main():
+    global capture
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640); 
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+    capture.set(cv2.CAP_PROP_SATURATION,0.2);
+    global img
+    try:
+        server = ThreadedHTTPServer(('localhost', 10088), CamHandler)
+        print( "server started")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
+
 
 class Main:
     def __init__(self):
@@ -34,14 +96,31 @@ class Main:
             if data:
                 data = json.loads(data.decode())
                 self.id = data['id']
-                print('registration complete, id is: {}\n'.format(self.id))
+                print(('registration complete, id is: {}\n'.format(self.id)))
                 return
+
+
 
     def activate_camera(self):
         if not self.camera_on:
             self.camera_on = True
-            p = Popen(['/usr/local/bin/streamer.sh'])
-            sleep(3)
+
+            global capture
+            capture = cv2.VideoCapture(0)
+            capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640); 
+            capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+            capture.set(cv2.CAP_PROP_SATURATION,0.2);
+            global img
+            try:
+                self.server = ThreadedHTTPServer(('localhost', 10088), CamHandler)
+                print( "server started")
+                self.server.serve_forever()
+            except KeyboardInterrupt:
+                capture.release()
+                server.socket.close()
+
+            print("It's all running now....")
+
             pkg = json.dumps({'msg': 'camera_on'});
             self.socket.send(pkg.encode())
         else: 
@@ -50,8 +129,10 @@ class Main:
     def deactivate_camera(self):
         if self.camera_on:
             self.camera_on = False 
-            call(['/usr/local/bin/stop_stream.sh'])
-            print('camera should be off')
+            
+            self.server.socket.close()
+
+            print('camera off')
             pkg = json.dumps({'msg': 'camera_off'});
             self.socket.send(pkg.encode())
         else:
@@ -65,11 +146,7 @@ class Main:
             #op knopje gedrukt
             if button.is_pressed and self.alarm_triggered == False:
                 self.alarm_triggered = True
-                
-                self.activate_camera();
-                
-                pkg = json.dumps({'msg': 'alarm'})
-                self.socket.send(pkg.encode())
+                activate_camera_thread = Thread(target=self.activate_camera, args=(True,), daemon=True)
     
     def keep_alive(self):
         while True:
@@ -98,7 +175,7 @@ class Main:
                 self.deactivate_camera()
             if data == 'camera_on':
                 print('camera_on')
-                print(self.camera_on)
+                print((self.camera_on))
                 self.activate_camera()
             if data == 'camera_off':
                 print('camera_off')
